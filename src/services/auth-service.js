2191ddd1d871fc156059a306105f0b591917efff
xxxxfault <jiaqian0730@gmail.com>
2022-11-22 12:03:56 -0500

    service for sign up

---
+++ src/services/auth-service.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+const BASE_URL = process.env.REACT_APP_BASE_URL
+
+const AUTH_API = `${BASE_URL}/api/auth`
+
+const api = axios.create({
+    withCredentials: true
+});
+
+export const signup = (user) =>
+    api.post(`${AUTH_API}/signup`, user)
+        .then(response => response.data);
